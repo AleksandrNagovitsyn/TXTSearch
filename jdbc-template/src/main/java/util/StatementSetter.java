b59faa088e4714ead95d0c578bc522a80f322194
User-312 <123>
2019-12-19 15:56:24 +0300

    removed'

--- jdbc-template/src/main/java/util/StatementSetter.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface StatementSetter {
-    public void set(PreparedStatement preparedStatement) throws SQLException;
-}
